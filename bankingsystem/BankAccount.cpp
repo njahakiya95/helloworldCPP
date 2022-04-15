32992724c1c512ba61480a60031ca6a88ccbd1d1
Nirmal Patel <patelnirmal1325@gmail.com>
2022-04-14 22:38:15 -0400

    updated Banking System project

--- bankingsystem/BankAccount.cpp
+++ bankingsystem/BankAccount.cpp
@@ -1,7 +1,16 @@
 #include "BankAccount.h"
-#include <iostream>
 using namespace std; 
 
 // BankAccount constructor
+BankAccount::BankAccount(string fname, string lname, int act_num, double bal) {
+    firstName = fname;
+    lastName = lname; 
+    act_number = act_num;
+    balance = bal; 
+}
 
-// BankAccount member functions
+// BankAccount class method defintions
+double Wthdraw(double wd_amount);  
+void Deposit(double dp_amount);     
+double CurrentBalance();            
+int ShowAccountInfo();              
--- bankingsystem/BankAccount.h
+++ bankingsystem/BankAccount.h
@@ -6,16 +6,20 @@ class BankAccount
 {
     //BankAccount class attributes 
     protected:
-        string firstName;   //stores first name
-        string lastName;    //stores last name
+        std::string firstName;   //stores first name
+        std::string lastName;    //stores last name
         int act_number;     //stores account number
         double balance;     //stores account balance
     
-    //BankAccount class method definitions 
+    //BankAccount class constructor and method declarations  
     public:
+        //BankAccount class constructor with zero-args 
+        BankAccount(std::string fname, std::string lname, int act_num = 0, double bal = 0); 
+        
+        //BankAccount class method declarations
         double Wthdraw(double wd_amount);   //Withdraw subtracts a user-input amount from balance
         void Deposit(double dp_amount);     //Deposit adds a user-input amount to balance
-        double CurrentBalance();            //CUrrentBalance shows the current account balance
+        double CurrentBalance();            //CurrentBalance shows the current account balance
         int ShowAccountInfo();              //ShowAccountInfo displays the first name, last name, and account number
 }; 
 
