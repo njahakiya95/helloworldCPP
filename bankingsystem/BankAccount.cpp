f20ce4a554b95484b6154b0dfb89cb37494a9dee
Nirmal Patel <patelnirmal1325@gmail.com>
2022-04-14 22:11:21 -0400

    updated Banking System project

--- bankingsystem/BankAccount.cpp
+++ bankingsystem/BankAccount.cpp
@@ -2,9 +2,6 @@
 #include <iostream>
 using namespace std; 
 
-class BankAccount {
-    //BankAccount class constructor definitions
-    
-    //BankAccount class method definitions
-    
-}
+// BankAccount constructor
+
+// BankAccount member functions
--- bankingsystem/BankAccount.h
+++ bankingsystem/BankAccount.h
@@ -1,18 +1,22 @@
 #ifndef BANKACCOUNT_H
 #define BANKACCOUNT_H
+#include <string>
 
-class BankAccount {
-//BankAccount class attributes 
-protected:
-    int act_number;            //stores account number 
-    float act_balance;      //stores account balance
+class BankAccount
+{
+    //BankAccount class attributes 
+    protected:
+        string firstName;   //stores first name
+        string lastName;    //stores last name
+        int act_number;     //stores account number
+        double balance;     //stores account balance
+    
+    //BankAccount class method definitions 
+    public:
+        double Wthdraw(double wd_amount);   //Withdraw subtracts a user-input amount from balance
+        void Deposit(double dp_amount);     //Deposit adds a user-input amount to balance
+        double CurrentBalance();            //CUrrentBalance shows the current account balance
+        int ShowAccountInfo();              //ShowAccountInfo displays the first name, last name, and account number
+}; 
 
-//BankAccount class constructor and method definitions 
-public:
-    BankAccount::BankAccount(int act_number, float act_balance);   //class constructor
-    void deposit(float dep_amount);                             //deposit() method is used to deposit money to act_balance
-    int get_act_number const;                                   //get_act_number() method is used to retrieve the act_number
-    float get_act_balance const;                                //get_act_balance() method is used to retrieve the act_balance 
-    
-    
-}
+#endif
