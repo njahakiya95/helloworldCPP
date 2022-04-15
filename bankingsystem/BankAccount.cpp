f764d3103fd6f34cd1139f1921622cd67e4b54e9
Nirmal Patel <patelnirmal1325@gmail.com>
2022-04-14 23:29:06 -0400

    updated Banking System project

--- bankingsystem/BankAccount.cpp
+++ bankingsystem/BankAccount.cpp
@@ -1,4 +1,5 @@
 #include "BankAccount.h"
+#include <iostream>
 using namespace std; 
 
 // BankAccount constructor
@@ -10,7 +11,21 @@ BankAccount::BankAccount(string fname, string lname, int act_num, double bal) {
 }
 
 // BankAccount class method defintions
-double Wthdraw(double wd_amount);  
-void Deposit(double dp_amount);     
-double CurrentBalance();            
-int ShowAccountInfo();              
+
+//Withdraw subtracts a user-input amount from balance
+double BankAccount::Wthdraw(double wd_amount) {
+    if (balance <= wd_amount) {
+        cout << "ERROR: CANNOT WITHDRAW VALUE\n"; 
+        cout << "Current balance is less than desired withdraw amount"; 
+    }
+    else {
+        balance -= wd_amount;
+        cout << "The new balance is\n"; 
+        return balance; 
+    }
+}
+
+//Deposit adds a user-input amount to balance
+void Deposit(dp_amount)  
+//double CurrentBalance();            
+//int ShowAccountInfo();              
